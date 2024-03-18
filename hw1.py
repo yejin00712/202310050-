87eefca80b5a778b7ede0e9ca190c88a9be550f8
yejin00712 <163308330+yejin00712@users.noreply.github.com>
2024-03-18 22:39:09 +0900

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -15,14 +15,3 @@ def get_circle_area():
 r = get_radius("넓이를 구하고자 하는 원의 반지름은? ")
 result = get_circle_area()
 print("반지름이", r, "인 원의 넓이 = 3.14 x", r, "x", r, "=", result)
-
-r = get_radius("넓이를 구하고자 하는 원의 반지름은? ")
-result = get_circle_area()
-print("반지름이", r, "인 원의 넓이 = 3.14 x", r, "x", r, "=", result)
-
-
-# In[ ]:
-
-
-
-
